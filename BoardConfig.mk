997799eca791bf67048f2178680ae7637f6acb65
tibixh <49487433+tibixh@users.noreply.github.com>
2022-02-10 22:13:46 +0100

    Added extra architecture stuff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,13 +1,40 @@
-# Inherit from common
-include device/samsung/msm8916-common/BoardConfigCommon.mk
-
 LOCAL_PATH := device/samsung/heatqlte
 
-# Assert
-TARGET_OTA_ASSERT_DEVICE := gprimeltexx,fortunafz,grandprimelte,grandprimeltexx
-
 # Kernel
-TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_fortuna_eur_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_heat_eur_defconfig
+
+BOARD_KERNEL_CMDLINE += \
+	console=null \
+	androidboot.hardware=qcom \
+	user_debug=23 \
+	msm_rtb.filter=0x3F \
+	ehci-hcd.park=3 \
+	androidboot.bootdevice=7824900.sdhci
+
+#BOARD_CUSTOM_BOOTIMG_MK := $(PLATFORM_PATH)/mkbootimg.mk
+BOARD_DTBTOOL_ARGS := -2
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
+BOARD_RAMDISK_OFFSET     := 0x02000000
+TARGET_KERNEL_CONFIG := msm8916_sec_defconfig
+TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
+TARGET_KERNEL_SELINUX_LOG_CONFIG := selinux_log_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/heatqlte
+
+
+#Arch
+TARGET_ARCH := arm
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := msm8916
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_SMP := true
+
+TARGET_BOOTLOADER_BOARD_NAME := heatqlte
 
 # Partition sizes
 BOARD_BOOTIMAGE_PARTITION_SIZE := 13631488
@@ -15,6 +42,7 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 15728640
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 5509201920
 BOARD_CACHEIMAGE_PARTITION_SIZE := 524288000
+BOARD_FLASH_BLOCK_SIZE := 131072
 
-# Properties
-TARGET_SYSTEM_PROP += $(LOCAL_PATH)/system.prop
+# ETC
+BOARD_HAS_NO_SELECT_BUTTON := true
