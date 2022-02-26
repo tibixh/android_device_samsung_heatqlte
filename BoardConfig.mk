9fc320bc3592d848d9340d8497ec29b949bbd41f
tibixh <tibi.nemeth12@gmail.com>
2022-02-26 22:55:19 +0100

    rework platform stuff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,27 +1,35 @@
 LOCAL_PATH := device/samsung/heatqlte
 
 # Kernel
-TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_heat_eur_defconfig
+# TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_heat_eur_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/heatqlte
+TARGET_KERNEL_CONFIG := heatqlte_defconfig
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_RAMDISK_OFFSET := 0x02000000
+BOARD_TAGS_OFFSET := 0x01e00000
+BOARD_SECOND_OFFSET := 0x00f0000
 
 BOARD_KERNEL_CMDLINE += \
 	console=null \
 	androidboot.hardware=qcom \
-	user_debug=23 \
+	user_debug=31 \
 	msm_rtb.filter=0x3F \
 	ehci-hcd.park=3 \
 	androidboot.bootdevice=7824900.sdhci
 
-BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/../msm8916-common/mkbootimg.mk
-BOARD_DTBTOOL_ARGS := -2
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
-BOARD_RAMDISK_OFFSET     := 0x02000000
-TARGET_KERNEL_CONFIG := msm8916_sec_defconfig
-TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
-TARGET_KERNEL_SELINUX_LOG_CONFIG := selinux_log_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/heatqlte
+# BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/../msm8916-common/mkbootimg.mk
+# BOARD_DTBTOOL_ARGS := -2
+# BOARD_KERNEL_BASE := 0x80000000
+# BOARD_KERNEL_PAGESIZE := 2048
+# BOARD_KERNEL_SEPARATED_DT := true
+# BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
+# BOARD_RAMDISK_OFFSET     := 0x02000000
+# TARGET_KERNEL_CONFIG := msm8916_sec_defconfig
+# TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
+# TARGET_KERNEL_SELINUX_LOG_CONFIG := selinux_log_defconfig
+# TARGET_KERNEL_SOURCE := kernel/samsung/heatqlte
 
 
 # Arch
@@ -34,10 +42,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_SMP := true
 
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-
-TARGET_BOOTLOADER_BOARD_NAME := heatqlte
+TARGET_BOOTLOADER_BOARD_NAME := msm8916
 
 # Partition sizes
 BOARD_BOOTIMAGE_PARTITION_SIZE := 13631488
--- device.mk
+++ device.mk
@@ -2,7 +2,7 @@
 $(call inherit-product-if-exists, vendor/samsung/heatqlte/heatqlte-vendor.mk)
 
 # Inherit from common
-$(call inherit-product, device/samsung/msm8916-common/msm8916.mk)
+$(call inherit-product-if-exists, device/samsung/msm8916-common/msm8916.mk)
 
 LOCAL_PATH := device/samsung/heatqlte
 
