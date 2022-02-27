0a7abe64e9e7701b2fe33ec707d8d0162a12a58e
tibixh <tibi.nemeth12@gmail.com>
2022-02-27 12:43:50 +0100

    modified mkbootimg,

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,19 +23,6 @@ BOARD_KERNEL_CMDLINE += \
 	ehci-hcd.park=3 \
 	androidboot.bootdevice=7824900.sdhci
 
-# BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/../msm8916-common/mkbootimg.mk
-# BOARD_DTBTOOL_ARGS := -2
-# BOARD_KERNEL_BASE := 0x80000000
-# BOARD_KERNEL_PAGESIZE := 2048
-# BOARD_KERNEL_SEPARATED_DT := true
-# BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
-# BOARD_RAMDISK_OFFSET     := 0x02000000
-# TARGET_KERNEL_CONFIG := msm8916_sec_defconfig
-# TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
-# TARGET_KERNEL_SELINUX_LOG_CONFIG := selinux_log_defconfig
-# TARGET_KERNEL_SOURCE := kernel/samsung/heatqlte
-
-
 # Arch
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
