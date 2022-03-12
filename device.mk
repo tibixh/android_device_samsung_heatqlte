67e4b102d64a5daef125640199a8fa01037daa43
tibixh <tibi.nemeth12@gmail.com>
2022-03-12 13:27:35 +0100

    added init packages cm should boot now?

--- device.mk
+++ device.mk
@@ -1,13 +1,31 @@
+LOCAL_PATH := device/samsung/heatqlte
+
 #Inherit from vendor
 $(call inherit-product-if-exists, vendor/samsung/heatqlte/heatqlte-vendor.mk)
 
 # Inherit from common
-$(call inherit-product-if-exists, device/samsung/msm8916-common/msm8916.mk)
+$(call inherit-product-if-exists, device/samsung/qcom-common/qcom-common.mk)
 
-LOCAL_PATH := device/samsung/heatqlte
+PRODUCT_PACKAGES += libtime_genoff
+
+# Init
+PRODUCT_PACKAGES += \
+    init.qcom.rc \
+    init.qcom.power.rc \
+    init.qcom.usb.rc \
+    init.recovery.qcom.rc \
+	init.crda.sh \
+    init.qcom.bt.sh \
+    init.qcom.coex.sh \
+    ueventd.qcom.rc
+
+PRODUCT_PACKAGES += \
+    services-ext \
+    init.cne.rc
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	ro.adb.secure=0 \
+	ro.debuggable=1
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
-# NFC configs
-
