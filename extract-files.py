5175081bb9e8681a8dc8af06affb4b65fad27ca6
tibixh <tibi.nemeth12@gmail.com>
2022-03-31 20:34:36 +0200

    rewrote setup files in python

---
+++ extract-files.py
@@ -0,0 +1,21 @@
+import shutil
+import os
+
+dumpdir = '/home/tibix/android/system_dumps/heatqlte/dump'
+propdir = '../../../vendor/samsung/heatqlte_test/proprietary'
+
+files = open('proprietary-files.txt').read().split('\n')
+
+for file in files:
+    if not(file.startswith('#') or file == ""):
+        filedir = file[0:-(len(file.split('/')[-1]) + 1)]
+        if not os.path.exists(f"{propdir}/{filedir}"):
+            print(f"Creating direcotry {propdir}/{filedir}")
+            os.makedirs(f'{propdir}/{filedir}')    
+        if os.path.exists(f"{dumpdir}/{file}"):
+            print(f"Copying {file}")
+            shutil.copyfile(f"{dumpdir}/{file}",f"{propdir}/{file}")
+        else:
+            print(f"No such file: {file}")
+print("extract-files.py done \nhere comes makefiles")
+os.system('python3 setup-makefiles.py')
