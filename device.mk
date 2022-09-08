1b32aebdc73d49ec182461746ebbd18d545d73c1
tibixh <tibi.nemeth12@gmail.com>
2022-09-08 23:40:06 +0200

    configs: add more media configs from stock

--- device.mk
+++ device.mk
@@ -88,7 +88,11 @@ PRODUCT_PACKAGES += \
 
 # Configs
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/audio_effects.conf:system/etc/audio_effects.conf \
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
 # Doze
 PRODUCT_PACKAGES += \
